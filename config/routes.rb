58e9149a0249ee80f3d4003e0ae06dce4fcc9726
AARODKB2012 <n00844015@unf.edu>
2021-04-29 15:20:13 -0400

    added changes to routes, testing works using bundle exec rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :colleges
-  scope '/api' do
-    resources :exam_windows
-    resources :users
-    resources :exams
-    end
+  resources :exam_windows
+  resources :users
+  resources :exams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
